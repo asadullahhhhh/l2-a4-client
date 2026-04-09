f6407a14fd6c48eea43d6d27b33af92dabc1b33c
Asadullah Al Galib <assadulla0172@gmail.com>
2026-04-09 20:24:07 +0600

    create the featured providers section in the home page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,22 +1,28 @@
 import { MealCard } from "@/components/shared/meal-card";
 import Banner from "@/modules/home/bannerSlider";
+import { ProviderCard } from "@/modules/providers/providerCard";
 import { homeService } from "@/service/home.service";
 import { Meal } from "@/types/meal.type";
+import { Provider } from "@/types/provider.type";
 import Image from "next/image";
 
 export default async function Home() {
 
-  const {data, error} = await homeService.getFeaturedMeals()
+  const {data: meals, error} = await homeService.getFeaturedMeals()
+  const {data: providers, error: providerError} = await homeService.getFeaturedProviders()
   
-  const featuredMeals = data?.data || []
+  const featuredMeals = meals?.data || []
+  const featuredProviders = providers?.data || []
 
-  console.log(featuredMeals);
 
   return (
     <div className="max-w-6xl mx-auto px-5">
+      {/* Banner Section */}
       <div>
         <Banner></Banner>
       </div>
+
+      {/* Featured Meals Section */}
       <div className="mt-30">
         <div className="mb-5">
           <h2 className="font-semibold text-4xl">Featured Meals</h2>
@@ -28,6 +34,20 @@ export default async function Home() {
               <MealCard meal={meal}></MealCard>
             )
           })
+        }
+        </div>
+      </div>
+
+      {/* Featured Providers Section */}
+      <div className="mt-30">
+        <div className="mb-5">
+          <h2 className="font-semibold text-4xl">Featured Providers</h2>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto max-w-6xl">
+          {
+          featuredProviders.map((provider: Provider) => (
+            <ProviderCard provider={provider}></ProviderCard>
+          ))
         }
         </div>
       </div>
--- src/service/home.service.ts
+++ src/service/home.service.ts
@@ -1,9 +1,10 @@
 import env from "@/env"
 import { cookies } from "next/headers"
 
+const cookieStore = await cookies()
+
 const getFeaturedMeals = async () => {
     try {
-        const cookieStore = await cookies()
         const response = await fetch(`${env.BACKEND_URL}/api/v1/home/featured-meals`, {
             method: "GET",
             headers: {
@@ -37,8 +38,41 @@ const getFeaturedMeals = async () => {
     }
 }
 
+const getFeaturedProviders = async () => {
+    try {
+        const response = await fetch(`${env.BACKEND_URL}/api/v1/home/featured-providers`, {
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: cookieStore.toString(),
+            }
+        })
 
+        const data = await response.json()
+
+        if (!response.ok) {
+            return {
+                data: null,
+                error: {
+                    message: data.message || "Failed to fetch featured providers.",
+                }
+            }
+        }
+
+        return {
+            data: data,
+            error: null,
+        }
+    } catch (error : any) {
+        return {
+            data: null,
+            error: {
+                message: error.message || "An error occurred while fetching featured providers.",
+            }
+        }
+    }
+}
 
 export const homeService = {
     getFeaturedMeals,
+    getFeaturedProviders
 }
